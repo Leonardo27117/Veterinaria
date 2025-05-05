a9ef031bdceec12a4aabaa84974ccb43389e82c1
Leonardo27117 <zS23014080@estudiantes.uv.mx>
2025-05-04 20:45:09 -0600

    Creacion de vistas

---
+++ Main.java
@@ -0,0 +1,11 @@
+package veterinaria;
+
+import veterinaria.vistas.Principal;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+        principal.setVisible(true);
+    }
+}
